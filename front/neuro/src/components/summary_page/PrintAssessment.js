c0a4ce488c5118660f81df7260f159f0309a1685
leo <leonardobocci99@hotmail.com>
2024-03-07 15:49:48 +0100

    fix printing button

--- front/neuro/src/components/summary_page/PrintAssessment.js
+++ front/neuro/src/components/summary_page/PrintAssessment.js
@@ -1,5 +1,5 @@
 // import * as React from 'react';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom'; 
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
@@ -8,15 +8,16 @@ import Alert from '@mui/material/Alert';
 
 const PrintAssessment = () => {
     const [showAlert, setShowAlert] = React.useState(false);
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
     function handleClick() {
         setShowAlert(true);
+        window.print();
     }
 
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '10vh' }}>
-            <Box sx={{ flex: '1 0 auto', padding: '0 2em', display: 'flex', justifyContent: 'center' }}>
-                <Link to="/summary" style={{ textDecoration: 'none' }}>
+        <Fragment>
+        <Box>
+        <Box sx={{ padding: '0 2em', display: 'flex', justifyContent: 'center' }}>
                     <Button
                         onClick={handleClick}
                         endIcon={<SendIcon />}
@@ -24,8 +25,9 @@ const PrintAssessment = () => {
                     >
                         <span>Save/Print</span>
                     </Button>
-                </Link>
             </Box>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '10vh' }}>
             <Box sx={{ padding: '0 2em', marginTop: 'auto' }}>
                 {showAlert && (
                     <Alert severity="success" onClose={() => setShowAlert(false)}>
@@ -34,6 +36,7 @@ const PrintAssessment = () => {
                 )}
             </Box>
         </Box>
+        </Fragment>
     )
 }
 export default PrintAssessment;
