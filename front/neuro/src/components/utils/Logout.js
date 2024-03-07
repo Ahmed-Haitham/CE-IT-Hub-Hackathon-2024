3d0ab10edefab385c39b3edf71c66e76d85ee0c0
Rzyski Maciej <rzyski.m@pg.com>
2024-03-07 17:14:27 +0100

    fixed issue with logout buton. Added admin panel button when logged in

--- front/neuro/src/components/utils/Logout.js
+++ front/neuro/src/components/utils/Logout.js
@@ -1,8 +1,11 @@
 // TODO: Logout in proper place
 import React, { useContext } from "react";
-import { Button } from '@mui/material';
+import { Button, ButtonGroup } from '@mui/material';
 import { UserContext } from "./UserContext";
+import { Link } from 'react-router-dom';
 
+
+// import { AppBar, Box, Toolbar, Button } from '@mui/material';
 const Logout = () => {
   const [, setToken] = useContext(UserContext);
 
@@ -11,11 +14,19 @@ const Logout = () => {
   };
 
   return (
-    <Button variant="contained" color="warning" onClick={handleLogout} style={{ textDecoration: 'none' }}>
+    <ButtonGroup variant="contained" aria-label="Basic button group">
+      <Button variant="contained" color="success" as={Link} to="/upload" style={{ textDecoration: 'none' }}>
+        ADMIN PANEL
+      </Button>
+      <Button variant="contained" color="warning" onClick={handleLogout} as={Link} to="/" style={{ textDecoration: 'none' }}>
         LOGOUT
-    </Button>
+      </Button>
+    </ButtonGroup>
 
-  );
+  )
+;
+  
+  ;
 };
 
 export default Logout;
