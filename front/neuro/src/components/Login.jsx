cd1cf692d104da70863dbd00b1d6046ec2405585
Rzyski Maciej <rzyski.m@pg.com>
2024-03-07 14:17:32 +0100

    login looks nicer

--- front/neuro/src/components/Login.jsx
+++ front/neuro/src/components/Login.jsx
@@ -1,16 +1,26 @@
-import React, { useState, useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import Header from "../Header";
 
-import ErrorMessage from "./ErrorMessage";
-import { UserContext } from "../context/UserContext";
 import { ThemeProvider } from '@mui/material/styles';
-import Theme from "../Theme"
+import Theme from "../Theme";
+import { UserContext } from "../context/UserContext";
+import ErrorMessage from "./ErrorMessage";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [errorMessage, seterrorMessage] = useState("");
-    const [, setToken] = useContext(UserContext);
+    const [token, setToken] = useContext(UserContext);
+    const navigate = useNavigate();
 
   const submitLogin = async () => {
     const requestOptions = {
@@ -25,54 +35,77 @@ const Login = () => {
       seterrorMessage(data.detail);
     } else {
       setToken(data.access_token);
+      navigate("/upload");
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     submitLogin();
+    
   };
 
     return (
         <React.StrictMode>
             <ThemeProvider theme={Theme}>
                 <Header />
-                <div className="column">
-                <form className="box" onSubmit={handleSubmit}>
-                    <h1 className="title has-text-centered">Login</h1>
-                    <div className="field">
-                    <label className="label">Username</label>
-                    <div className="control">
-                        <input
-                        type="username"
-                        placeholder="Enter username"
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                    >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit}  noValidate sx={{ mt: 1 }}>
+                        <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        className="input"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        className="input"
-                        required
+                        autoComplete="username"
+                        autoFocus
                         />
-                    </div>
-                    </div>
-                    <div className="field">
-                    <label className="label">Password</label>
-                    <div className="control">
-                        <input
+                        <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
                         type="password"
-                        placeholder="Enter password"
+                        className="input"
+                        id="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        className="input"
-                        required
+                        autoComplete="current-password"
                         />
-                    </div>
-                    </div>
-                    <ErrorMessage message={errorMessage} />
-                    <br />
-                    <button className="button is-primary" type="submit">
-                    Login
-                    </button>
-                </form>
-                </div>
+                        <ErrorMessage message={errorMessage} />
+                        <br />
+                        <Button
+                        className="button is-primary"
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        >
+                        Sign In
+                        </Button>
+                    </Box>
+                    </Box>
+                </Container>
             </ThemeProvider>
         </React.StrictMode>
     );
